76c481a71736c05e44258957212658c40c33ee54
derrick12 <derrickwaweru@gmail.com>
2018-08-10 14:46:21 +0300

    add equilateral triangle function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 var a = parseInt(prompt("Enter the side of A"));
 var b = parseInt(prompt("Enter the side of B"));
 var c = parseInt(prompt("Enter the side of C"));
+
+if (a === b && b === c) {
+  alert("This is an equilateral Triangle");
+}
