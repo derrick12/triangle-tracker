d21b5e2ec693f8bf7f81fdc4c861349aee1aa86a
derrick12 <derrickwaweru@gmail.com>
2018-08-10 15:18:15 +0300

    add leTriangle function

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
-var a = parseInt(prompt("Enter the side of A"));
-var b = parseInt(prompt("Enter the side of B"));
-var c = parseInt(prompt("Enter the side of C"));
+function leTriangle() {
+  var a = parseInt(prompt("Enter the side of A"));
+  var b = parseInt(prompt("Enter the side of B"));
+  var c = parseInt(prompt("Enter the side of C"));
 
-if (a === b && b === c) {
-  alert("This is an equilateral Triangle");
-}
+  if (a === b && b === c) {
+    alert("This is an equilateral Triangle");
+  }
 
-else if (a === b || b===c || a===c ) {
-  alert("This is an isosceles Triangle");
-}
+  else if (a === b || b===c || a===c ) {
+    alert("This is an isosceles Triangle");
+  }
 
-else if ( a+b>c || b+c>=a || a+c<=b ) {
-  alert("This is an scalene Triangle");
-}
+  else if ( a+b>c || b+c>=a || a+c<=b ) {
+    alert("This is an scalene Triangle");
+  }
 
-else ( a+b<=c || b+c<=a || a+c<=b ){
-  alert("This is not a tringle");
+  else ( a+b<=c || b+c<=a || a+c<=b ){
+    alert("This is not a tringle");
+  }
 }
